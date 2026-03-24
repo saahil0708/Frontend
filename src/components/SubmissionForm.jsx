611270dd612162e42511738f172a8ea28901bec8
saahil0708 <saahil.ind.dgp@gmail.com>
2026-03-24 14:41:56 +0530

    ..

--- src/components/SubmissionForm.jsx
+++ src/components/SubmissionForm.jsx
@@ -49,7 +49,8 @@ const SubmissionForm = ({ closePopup }) => {
     data.append("pdf", file);
 
     try {
-      const response = await axios.post("http://localhost:5000/api/submit", data, {
+      const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
+      const response = await axios.post(`${apiUrl}/api/submit`, data, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
