a9b56b9075cbec8720411c071ee5b534e1209bfc
saahil0708 <saahil.ind.dgp@gmail.com>
2026-03-27 10:15:25 +0530

    .

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -10,6 +10,10 @@ const Countdown = () => {
     const now = new Date().getTime();
     const distance = targetDate - now;
 
+    if (distance <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
     return {
       days: Math.floor(distance / (1000 * 60 * 60 * 24)),
       hours: Math.floor((distance / (1000 * 60 * 60)) % 24),
